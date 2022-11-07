71c11de5064e5c10c4b4459bb2051ac8ead77836
TereseBo~ <terese.boderus@yh.nackademin.se>
2022-11-07 19:05:10 +0100

    Added function for adjusting grid when increasing thredles/shafts

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 
 
-function setUserPreferences() {
-    let shaftInput = document.getElementById('shafts').value;
-    let thredleInput = document.getElementById('thredles').value;
-    console.log(shaftInput, thredleInput);
-    localStorage.setItem('shaftInput', shaftInput);
-    localStorage.setItem('thredleInput', thredleInput);
-    
-}
 
+//Functions for collecting user preferences
 function removeMenu() {
     let menu = document.getElementById('menu');
     while (menu.firstChild) {
@@ -50,10 +43,24 @@ function createForm() {//Creates form for user to input preferences
         
     })
 }
+function setUserPreferences() {
+    let shaftInput = document.getElementById('shafts').value;
+    let thredleInput = document.getElementById('thredles').value;
+    console.log(shaftInput, thredleInput);
+    localStorage.setItem('shaftInput', shaftInput);
+    localStorage.setItem('thredleInput', thredleInput);
+    
+}
+function collectUserPreferences() {
+    removeMenu();
+    createForm();
+
+}
+//Functions for greating draft
+
 function createGrid(x=4,y=4, id="grid") {//Skapar ett table-element med x antal rader och y antal kolumner och id id.
-    //removeMenu();
-    console.log(x,y);
-    console.log('creating grid');
+   
+
     let grid = document.createElement('div');
 
     grid.setAttribute("id", `${id}-wrapper`);
@@ -67,54 +74,38 @@ function createGrid(x=4,y=4, id="grid") {//Skapar ett table-element med x antal 
         for (let j = 0; j < x; j++) {
             let cell = document.createElement("div");
             cell.setAttribute("class", "cell");
-            cell.setAttribute("id", `${id}:${j},${i}`);
+            let cellName =  `${id}:${j},${i}`;
+            cell.setAttribute("id", cellName);
            cell.addEventListener('click', event => {
                //TODO: add effects of click
-                console.log(`click on cell ${document.getElementById(`${id}:${j},${i}`).id}`);
+                console.log(`click on cell ${document.getElementById(cellName).id}`);
+                console.log(cellName);
+                getActiveColor();
+                console.log(this);
             })
             row.appendChild(cell);
         }
         grid.appendChild(row);
     }
 }
-function adjustDraftDisplay(gridSizeX, gridSizeY, thredlePreference, shaftPreference){
+function adjustDraftDisplay(gridSizeX, gridSizeY, thredlePreference, shaftPreference){//Adjusts the size of the draft grid to prevent rezising of cells when adding shafts/thredles.
     let el=document.getElementById('content');
     let draft=document.getElementById('draft-wrapper');
     let shafts=document.getElementById('shafts-wrapper');
     let thredles=document.getElementById('thredles-wrapper');
     let tieUp=document.getElementById('tie-up-wrapper');
-    let adjustedX=gridSizeX+1;
-    let adjustedY=gridSizeY+1;
+    gridSizeX=parseInt(gridSizeX);
+    gridSizeY=parseInt(gridSizeY);
+ //Adjusts the end columns/rows of the grid to accomodate for the number of shafts/thredles
     thredlePreference=parseInt(thredlePreference);
     shaftPreference=parseInt(shaftPreference);
-   // console.log(`adjustedX ${adjustedX} typer of: ${typeof(adjustedX)}, adjustedY ${adjustedY} typer of: ${typeof(adjustedY)}, thredlePreference ${thredlePreference} typer of: ${typeof(thredlePreference)}, shaftPreference ${shaftPreference} typer of: ${typeof(shaftPreference)}`);
-    el.style.display="grid";
-    el.style.gridTemplateColumns=`repeat(${adjustedX + thredlePreference}, 1rem)` ;
-    el.style.gridTemplateRows=`repeat(${adjustedY + shaftPreference}, 1rem)`;
-    el.style.gap="1rem";
-
-    draft.style.gridColumn=`1 / ${adjustedX}`;
-    draft.style.gridRow=`1 / ${adjustedY}`;
-
-    shafts.style.gridColumn= draft.style.gridColumn;
-    shafts.style.gridRow=`${adjustedY} / ${adjustedY + shaftPreference+1 }`;
-
-    thredles.style.gridColumn= `${adjustedX} / ${adjustedX + thredlePreference+1}`;
-    thredles.style.gridRow= draft.style.gridRow;
-
-    tieUp.style.gridColumn= thredles.style.gridColumn;
-    tieUp.style.gridRow= shafts.style.gridRow;
-
-    console.log(`el.style.gridTempColumn: ${el.style.gridTemplateColumns}, el.style.gridTempRow: ${el.style.gridTemplateRows}`);
-    console.log(`draft.style.gridColumn: ${draft.style.gridColumn}, draft.style.gridRow: ${draft.style.gridRow}`);
-    console.log(`shafts.style.gridColumn: ${shafts.style.gridColumn}, shafts.style.gridRow: ${shafts.style.gridRow}`);
-    console.log(`thredles.style.gridColumn: ${thredles.style.gridColumn}, thredles.style.gridRow: ${thredles.style.gridRow}`);
-    console.log(`tieUp.style.gridColumn: ${tieUp.style.gridColumn}, tieUp.style.gridRow: ${tieUp.style.gridRow}`);
-
-
-
+    let draftAreaWidth=gridSizeX+thredlePreference+2;
+    let draftAreaHeight=gridSizeY+shaftPreference+2;
+    shafts.style.gridRowEnd=(draftAreaHeight);
+    thredles.style.gridColumnEnd=(draftAreaWidth);
+    tieUp.style.gridRowEnd=(draftAreaHeight);
+    tieUp.style.gridColumnEnd=(draftAreaWidth);
 }
-
 function draftSetUp() {//Checks if user has set thredle/shaft preferences, if not uses 4 as default. Navigation to draft.html and creation og grid.
     let shaftPreference;
     let thredlePreference;
@@ -126,14 +117,13 @@ function draftSetUp() {//Checks if user has set thredle/shaft preferences, if no
     createGrid(gridSizeX,shaftPreference, "shafts");
     createGrid(thredlePreference,gridSizeY, "thredles");
     createGrid(thredlePreference,shaftPreference, "tie-up");
-   // adjustDraftDisplay(gridSizeX, gridSizeY, thredlePreference, shaftPreference);
- 
     
-    
-    
+    adjustDraftDisplay(gridSizeX, gridSizeY, thredlePreference, shaftPreference);
 }
-function collectUserPreferences() {
-    removeMenu();
-    createForm();
+//Functions in the actual draft
+function getActiveColor() {
+   let currentColor=document.getElementById('currentcolor').value;
+   console.log(currentColor);
+}
 
-}
+
