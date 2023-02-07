48be52cb0109bfae184a95b40bed1edf16b69aba
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-02-07 14:12:06 +0100

    added previously chosen yarn

--- scripts/colorpick.js
+++ scripts/colorpick.js
@@ -3,28 +3,23 @@ function addOptionColor(rgb, thread) {//adds a color to the color pto the previo
     let container=document.getElementById(`${thread}-box`);
     option.id=rgb;
     option.classList.add('thread');
-    option.value=rgb;
     option.style.backgroundColor=rgb;
+    option.addEventListener('click', ()=>{setColorPicker(rgb)});
     container.appendChild(option);
 }
 function displayPreviousColors(thread){//displays the previous colors in the color select
     let container=document.getElementById(`previous-colors-container`);
-    console.log(container);
     let select=document.getElementById(`${thread}-color-container`);
    select.removeAttribute('style');
    container.removeAttribute("style");
 }
 function setColorPicker(rgb){
-    document.getElementById('color-picker').value=rgb;
+    document.getElementById('currentcolor').value=rgb;
 }
 
 function isNewColor(rgb, thread) {//checks if the color is new
     let select=document.getElementById(`${thread}-box`);
-    console.log(select);
     let options=select.querySelectorAll('div');
-    console.log(options);
-    console.log(Array.from(options));
-    console.log(Array.from(options).find(option=>option.id===rgb));
     if(Array.from(options).find(option=>option.id===rgb)){
         return false;
     }
