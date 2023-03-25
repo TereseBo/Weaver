29607e19d7cf73b552d54e4580afcc01a116ba69
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-03-25 09:24:47 +0100

    added objecttemplate, started functions for object import

--- scripts/draft/filescripts/read.js
+++ scripts/draft/filescripts/read.js
@@ -1,2 +1,26 @@
+import { draftSetUp } from "../pagescripts/dchart";
+
+
+function removeGrid() {//Not tested
+    let chart = document.getElementById;
+    while (chart.firstChild) {
+        chart.removeChild(chart.lastChild);
+    }
+}
+
+function importcolors(object){
+    let shafts=document.getElementById('shafts')
+    object.shafts.pattern.forEach(element, index => {
+        shafts.getElementById(element)
+
+    });
+}
+
+function createDraftFromObject(object) {
+    draftSetUp(object.shafts.count, object.threadling.count);
+}
 
 //page import
+//remove grid
+//run draft setup with object values
+//add colors
